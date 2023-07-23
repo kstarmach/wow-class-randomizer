6b1214ff826f1e62ed5f3fd4ee25cb141ea85608
Kamil Starmach <kstarmach@outlook.com>
2023-07-23 15:24:35 +0200

    Removed animation becouse 'motion sickness'

--- app/expansions.tsx
+++ app/expansions.tsx
@@ -1,9 +1,10 @@
+"use client"
 import Image from 'next/image'
+import { useState } from 'react'
 
 const Expansion = ({ logo, background }: { logo: string, background: string }) => {
 
     return (
-
         <div className="section grayscale flex justify-center items-center hover-scale " style={{ backgroundImage: `url('/${background}')` }}>
             <div className="text-white text-center p-8">
                 <Image
@@ -19,10 +20,45 @@ const Expansion = ({ logo, background }: { logo: string, background: string }) =
     )
 }
 
-const Expansions = () => {
-    return (
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 h-screen relative ">
+const FullScreenDiv: React.FC = () => {
+    const [isFullScreen, setIsFullScreen] = useState(false);
+
+    const handleFullScreen = () => {
+        setIsFullScreen((prevState) => !prevState);
+    };
+
+    return (
+        <div
+            id="fullScreenDiv"
+            className={`border p-4 cursor-pointer ${isFullScreen ? 'fullscreen' : ''}`}
+            onClick={handleFullScreen}
+        >
+            Click to go full screen
+        </div>
+    );
+};
+
+const Expansions = () => {
+    const [isFullScreen, setIsFullScreen] = useState(false);
+
+    const handleFullScreen = () => {
+        setIsFullScreen((prevState) => !prevState);
+    };
+
+    if (isFullScreen) {
+        return (
+            <div className={`grid grid-cols-1 h-screen relative`} onClick={handleFullScreen}>
+                <Expansion
+                    logo="World-of-Warcraft-Logo.png"
+                    background="dragonflight.jpg"
+                />
+            </div>
+        )
+    }
+
+    return (
+        <div className={`grid grid-cols-3 h-screen relative ${isFullScreen ? 'hidden' : ''}`} onClick={handleFullScreen}>
 
             <Expansion
                 logo="wowclassichardcore.png"
